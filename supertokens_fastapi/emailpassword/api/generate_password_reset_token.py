8207cd7826396401ac96f3d773629151c1b256d0
kl-sento <konstantin@sento.io>
2021-07-14 12:20:22 +0200

    Move json import order

--- supertokens_fastapi/emailpassword/api/generate_password_reset_token.py
+++ supertokens_fastapi/emailpassword/api/generate_password_reset_token.py
@@ -13,8 +13,8 @@ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 License for the specific language governing permissions and limitations
 under the License.
 """
-import json
 from __future__ import annotations
+import json
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from supertokens_fastapi.emailpassword.recipe import EmailPasswordRecipe
