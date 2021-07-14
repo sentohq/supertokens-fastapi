a1b5f4af5395b947d6a7b4ca5a71ff3677d22ac8
Nikolai Kraler <me@nikolai-kraler.de>
2021-07-14 15:13:15 +0200

    implement supertokens_session method similar to V1.1

--- supertokens_fastapi/session/middleware.py
+++ supertokens_fastapi/session/middleware.py
@@ -14,11 +14,10 @@ License for the specific language governing permissions and limitations
 under the License.
 """
 from __future__ import annotations
-from typing import Union, TYPE_CHECKING
+from typing import Union
 from supertokens_fastapi.utils import normalise_http_method
-if TYPE_CHECKING:
-    from .session_class import Session
-    from .session_recipe import SessionRecipe
+from .session_class import Session
+from .session_recipe import SessionRecipe
 from fastapi.requests import Request
 from supertokens_fastapi.normalised_url_path import NormalisedURLPath
 
@@ -37,3 +36,30 @@ def verify_session(recipe: SessionRecipe, anti_csrf_check: Union[bool, None] = N
         return request.state.supertokens
 
     return func
+
+
+async def supertokens_session(request: Request):
+    enable_anti_csrf_check = request.method != "GET"
+    return await __supertokens_session(request, enable_anti_csrf_check)
+
+
+async def supertokens_session_with_anti_csrf(request: Request):
+    return await __supertokens_session(request, True)
+
+
+async def supertokens_session_without_anti_csrf(request: Request):
+    return await __supertokens_session(request, False)
+
+
+async def __supertokens_session(request: Request, anti_csrf_check: bool) -> Union[Session, None]:
+    recipe = SessionRecipe.get_instance()
+    method = normalise_http_method(request.method)
+    if method == 'options' or method == 'trace':
+        return None
+    incoming_path = NormalisedURLPath(recipe, request.url.path)
+    refresh_token_path = recipe.config.refresh_token_path
+    if incoming_path.equals(refresh_token_path) and method == 'post':
+        request.state.supertokens = await recipe.refresh_session(request)
+    else:
+        request.state.supertokens = await recipe.get_session(request, anti_csrf_check, True)
+    return request.state.supertokens
